2a1e9e7400ae468e7b23c53ccb30bee026b9f920
Drescher, Maximilian <maximilian.drescher@student.fhws.de>
2021-04-10 15:21:49 +0200

    insert title

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class Cheerganize extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title:'Cheerganize',
       theme: ThemeData.light().copyWith(
           primaryColor: Color(0xffb6a24f),
           scaffoldBackgroundColor: Color(0xFF181d21),
